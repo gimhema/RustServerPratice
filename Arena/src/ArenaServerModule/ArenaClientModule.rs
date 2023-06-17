fdb6d31de8637607815683df65428adcd5516f89
gimhema <49871739+gimhema@users.noreply.github.com>
2023-06-17 14:27:58 +0900

    Update ArenaClientModule.rs
    Add Client Struct and Impl Abstract

--- Arena/src/ArenaServerModule/ArenaClientModule.rs
+++ Arena/src/ArenaServerModule/ArenaClientModule.rs
@@ -1,3 +1,19 @@
 pub fn Initailize() {
     println!("Arena Client Initialize");
 }
+
+pub struct ArenaClient {
+    userID: i64,
+    userPW: String,
+    userName: String,
+    connectedIPAddress: String,
+}
+
+impl  ArenaClient {
+    pub fn Create() {
+
+    }
+    pub fn Connect() {
+
+    }
+}
