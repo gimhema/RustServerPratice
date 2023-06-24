b25f6d31944a14d64b47395678ee0fee3e32587c
gimhema <49871739+gimhema@users.noreply.github.com>
2023-06-24 13:10:38 +0900

    Add some class about client . . .
    add connectinfo, client class, manager etc . . .

--- Arena/src/ArenaServerModule/ArenaClientModule.rs
+++ Arena/src/ArenaServerModule/ArenaClientModule.rs
@@ -1,19 +1,62 @@
+use std::collections::HashMap;
+use mio::net::{TcpListener, TcpStream};
+use mio::{Token};
+
 pub fn Initailize() {
     println!("Arena Client Initialize");
 }
+
+pub struct ConnectInfo {
+    connectToken: Token,
+    connectStream: TcpStream
+}
+
+impl ConnectInfo {
+    pub fn Create(&mut self, token: Token, stream: TcpStream) 
+    {
+        self.connectToken = token;
+    }
+}
+
 
 pub struct ArenaClient {
     userID: i64,
     userPW: String,
     userName: String,
     connectedIPAddress: String,
+    connectToken: Token,
+    connectStream: TcpStream
 }
 
 impl  ArenaClient {
-    pub fn Create() {
-
+    pub fn Create(&mut self, ID: i64, Password: String, userName: String, IP: String, token : Token, stream: TcpStream) {
+        self.userID = ID;
+        self.userPW = Password;
+        self.userName = userName;
+        self.connectedIPAddress = IP;
+        self.connectToken = token;
+        self.connectStream = stream;
     }
     pub fn Connect() {
 
     }
 }
+
+pub struct ArenaClientManager {
+    clientContainer: HashMap<Token, ArenaClient>
+}
+
+impl ArenaClientManager {
+    pub fn Initialize(&mut self)
+    {
+        println!("Arena Client Manager Initialize");
+        
+        self.clientContainer = HashMap::new();
+    }
+
+    pub fn AddNewUserToContainer(&mut self, connectToken: Token, newClient: ArenaClient)
+    {
+        self.clientContainer.insert(connectToken, newClient);
+    }
+}
+
