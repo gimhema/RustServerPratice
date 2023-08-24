8fb0fa000c476b76ccf998a0f0e572a4adc2cdf5
gimhema <49871739+gimhema@users.noreply.github.com>
2023-08-25 01:07:46 +0900

    Update ArenaClientModule.rs
    멤버에 대한 getter&setter

--- Arena/src/ArenaServerModule/ArenaClientModule.rs
+++ Arena/src/ArenaServerModule/ArenaClientModule.rs
@@ -39,6 +39,38 @@ impl  ArenaPlayer {
         self.userID = ID;
         self.userName = userName;
     }
+
+    pub fn get_userID(&self) -> &i64 {
+        &self.userID
+    }
+
+    pub fn get_userName(&self) -> &String {
+        &self.userName
+    }
+
+    pub fn get_playerLogic(&self) -> &GamePlayerLogic {
+        &self.playerLogic
+    }
+
+    pub fn get_userToken(&self) -> &Token {
+        &self.userToken
+    }
+
+    pub fn set_userID(&mut self, new_id: i64) {
+        self.userID = new_id;
+    }
+
+    pub fn set_userName(&mut self, new_name: String) {
+        self.userName = new_name;
+    }
+
+    pub fn set_playerLogic(&mut self, new_logic: GamePlayerLogic) {
+        self.playerLogic = new_logic;
+    }
+
+    pub fn set_userToken(&mut self, new_token: Token) {
+        self.userToken = new_token;
+    }
 }
 
 
