d147293a2ca8e83178214af1fab42caf05a45673
gimhema <49871739+gimhema@users.noreply.github.com>
2023-10-08 01:44:25 +0900

    Create ArenaGameMode
    .

---
+++ UnrealClient/UnrealArenaClient/Source/UnrealArenaClient/GameCommon/ArenaGameMode.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+
+#include "ArenaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALARENACLIENT_API AArenaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
