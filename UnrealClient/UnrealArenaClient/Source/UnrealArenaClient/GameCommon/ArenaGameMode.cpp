419296449e3f4073a80558947248dfea8b5da105
gimhema <49871739+gimhema@users.noreply.github.com>
2023-10-08 01:47:11 +0900

    .
    .

--- UnrealClient/UnrealArenaClient/Source/UnrealArenaClient/GameCommon/ArenaGameMode.cpp
+++ UnrealClient/UnrealArenaClient/Source/UnrealArenaClient/GameCommon/ArenaGameMode.cpp
@@ -3,3 +3,19 @@
 
 #include "ArenaGameMode.h"
 
+void AArenaGameMode::InitializeFunctionMap()
+{
+	MessageFuncMap.Add(EArenaGameMessage::ECONSOLE_WRITE_LINE, CreateMessageFunc("RECV_ECONSOLE_WRITE_LINE_FUNC"));
+}
+
+void AArenaGameMode::RECV_ECONSOLE_WRITE_LINE_FUNC()
+{
+
+}
+
+FGameMessageFuncDelegate AArenaGameMode::CreateMessageFunc(FString funcName)
+{
+	FGameMessageFuncDelegate _delegate;
+	_delegate.BindUFunction(this, FName(funcName));
+	return _delegate;
+}
--- UnrealClient/UnrealArenaClient/Source/UnrealArenaClient/GameCommon/ArenaGameMode.h
+++ UnrealClient/UnrealArenaClient/Source/UnrealArenaClient/GameCommon/ArenaGameMode.h
@@ -4,15 +4,30 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
-
+#include "../Network/ArenaGameMessage.h"
 #include "ArenaGameMode.generated.h"
 
 /**
  * 
  */
+
+DECLARE_DELEGATE(FGameMessageFuncDelegate);
+
 UCLASS()
 class UNREALARENACLIENT_API AArenaGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+
+public:
+	TMap<EArenaGameMessage, FGameMessageFuncDelegate> MessageFuncMap;
+
+public:
+	void InitializeFunctionMap();
+	FGameMessageFuncDelegate CreateMessageFunc(FString funcName);
+
+public:
+	// Function Maps . . . .
+	void RECV_ECONSOLE_WRITE_LINE_FUNC();
+
 };
