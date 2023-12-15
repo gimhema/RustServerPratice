aa09fc3b0678a4a5164251427040a41bd52c8803
gimhema <49871739+gimhema@users.noreply.github.com>
2023-12-15 23:10:48 +0900

    Update ServerBaseModule.rs
    .

--- Migration/BaseServer/src/Server/ServerBaseModule.rs
+++ Migration/BaseServer/src/Server/ServerBaseModule.rs
@@ -26,7 +26,8 @@ fn next(current: &mut Token) -> Token {
 }
 
 pub struct ServerBase {
-
+    recvMessageBuffer: Mutex<VecDeque<String>>,
+    sendMessageBuffer: Mutex<VecDeque<String>> // need change String -> GamePacket
 }
 
 impl ServerBase {
