98e6899b84df7d1c43478a3aec6e0320cae399bd
gimhema <49871739+gimhema@users.noreply.github.com>
2023-12-16 19:10:05 +0900

    create packet module
    create packet module

---
+++ Migration/BaseServer/src/Server/GamePacketModule.rs
@@ -0,0 +1,10 @@
+
+use serde::{Serialize, Deserialize};
+use serde_json::{to_string, from_str};
+
+
+pub struct GamePacket {
+
+}
+
+
--- Migration/BaseServer/src/Server/mod.rs
+++ Migration/BaseServer/src/Server/mod.rs
@@ -1,6 +1,8 @@
 pub mod ServerBaseModule;
 pub mod ServerFunctions;
+pub mod GamePacketModule;
 
 pub mod Server {
     use super::ServerBaseModule::ServerBase;
+    use super::GamePacketModule::GamePacket;
 }
