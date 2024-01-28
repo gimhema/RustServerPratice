a3b1ee47eec9e9bb113e9f497bc4915af36c80df
gimhema <49871739+gimhema@users.noreply.github.com>
2024-01-28 02:05:06 +0000

    Create Message Prompt Client for test message send and recv

---
+++ MessagePrompt/MessagePrompt/target/debug/MessagePrompt.d
@@ -0,0 +1,1 @@
+/home/ws_rust/Server/RustServerPratice/MessagePrompt/MessagePrompt/target/debug/MessagePrompt: /home/ws_rust/Server/RustServerPratice/MessagePrompt/MessagePrompt/src/main.rs
---
+++ MessagePrompt/MessagePrompt/target/debug/deps/MessagePrompt-115b648aa4f11861.d
@@ -0,0 +1,5 @@
+/home/ws_rust/Server/RustServerPratice/MessagePrompt/MessagePrompt/target/debug/deps/MessagePrompt-115b648aa4f11861: src/main.rs
+
+/home/ws_rust/Server/RustServerPratice/MessagePrompt/MessagePrompt/target/debug/deps/MessagePrompt-115b648aa4f11861.d: src/main.rs
+
+src/main.rs:
