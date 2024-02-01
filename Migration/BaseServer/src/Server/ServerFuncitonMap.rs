a1a18ca58d1f234192cf274539af0aae9f85e78a
gimhema <49871739+gimhema@users.noreply.github.com>
2024-02-01 22:09:15 +0900

    Update ServerFuncitonMap.rs
    .

--- Migration/BaseServer/src/Server/ServerFuncitonMap.rs
+++ Migration/BaseServer/src/Server/ServerFuncitonMap.rs
@@ -11,9 +11,11 @@ pub fn ServerAction_CHAT_MESSAGE_ALL(server: &mut ServerBase, val : GamePacket) 
     let mut result = FunctionCallResult::FUNCTION_CALL_FAIL;
     let mut _numUser = server.GetNumUser().clone();
 
-    if (_numUser <= 0)
-    {
+    if (_numUser <= 0) {
         result = FunctionCallResult::FUNCTION_CALL_FAIL;
+    }
+    else {
+        result = FunctionCallResult::FUNCTION_CALL_SUCCESS;        
     }
 
     let mut _sendID = val.getSenderID();
@@ -32,12 +34,44 @@ pub fn ServerAction_CHAT_MESSAGE_ALL(server: &mut ServerBase, val : GamePacket) 
     result
 }
 
-pub fn ServerAction_CHAT_MESSAGE_TO_ONE(server: &mut ServerBase, val : GamePacket) -> i64 {
+pub fn ServerAction_CHAT_MESSAGE_TO_ONE(server: &mut ServerBase, val : GamePacket) -> FunctionCallResult {
     // println!("Message Test 2 {}",  val);
-    0
+    let mut result = FunctionCallResult::FUNCTION_CALL_FAIL;
+    let mut _numUser = server.GetNumUser().clone();
+
+    if (_numUser <= 0) {
+        result = FunctionCallResult::FUNCTION_CALL_FAIL;
+    }
+    else {
+        result = FunctionCallResult::FUNCTION_CALL_SUCCESS;        
+    }
+
+    let mut _sendID = val.getSenderID();
+    let mut _targetID = val.getTargetID();
+    let mut _fParamVec = val.getFunctionParam();
+    let mut _fParamStr = val.getFunctionStrParam();
+    let mut _fHeader = val.getFunctionHeader();
+
+    let mut _packet = GamePacket::new(*_targetID, *_sendID,
+        *_fHeader, _fParamVec.clone(), _fParamStr.clone() );
+
+    gSendMessageBuffer.PushBackData(_packet);
+
+    result
 }
 
-pub fn ServerAction_CHAT_MESSAGE_TO_GROUP(server: &mut ServerBase, val : GamePacket) -> i64 {
+pub fn ServerAction_CHAT_MESSAGE_TO_GROUP(server: &mut ServerBase, val : GamePacket) -> FunctionCallResult {
     // println!("Message Test 3 {}",  val);
-    0
+    let mut result = FunctionCallResult::FUNCTION_CALL_FAIL;
+    let mut _numUser = server.GetNumUser().clone();
+
+    if (_numUser <= 0) {
+        result = FunctionCallResult::FUNCTION_CALL_FAIL;
+    }
+    else {
+        result = FunctionCallResult::FUNCTION_CALL_SUCCESS;        
+    }
+
+
+    result
 }
