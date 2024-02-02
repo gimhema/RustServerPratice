3619e5ffce533655e19bd1c24959f779404b4f05
gimhema <49871739+gimhema@users.noreply.github.com>
2024-02-02 23:41:34 +0900

    .
    .

---
+++ Migration/BaseServer/src/GameLogic/GameLogicBaseModule.rs
@@ -0,0 +1,13 @@
+
+
+
+
+
+struct GameLogicBase {
+
+}
+
+impl GameLogicBase {
+
+}
+
---
+++ Migration/BaseServer/src/GameLogic/mod.rs
@@ -0,0 +1,9 @@
+pub mod GamePacketModule;
+
+
+
+use crate::{gSendMessageBuffer, gRecvMessageBuffer, gServer};
+
+pub mod GameLogic {
+
+}
