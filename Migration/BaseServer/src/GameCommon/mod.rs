3e13a223fbde88285c93c9e817ccbaf7966ee15a
gimhema <49871739+gimhema@users.noreply.github.com>
2024-02-09 15:02:13 +0000

    Update character module, Create math module

--- Migration/BaseServer/src/GameCommon/mod.rs
+++ Migration/BaseServer/src/GameCommon/mod.rs
@@ -1,4 +1,5 @@
 pub mod Manager;
+pub mod Math;
 
 pub mod GameCommon {
 
--- Migration/BaseServer/src/GameLogic/CharacterModule.rs
+++ Migration/BaseServer/src/GameLogic/CharacterModule.rs
@@ -1,13 +1,37 @@
 use crate::GameCommon::Manager::*;
-
+use crate::GameCommon::Math::*;
+use std::collections::VecDeque;
 
 pub struct Character {
+    pid : i64,
+    name : String,
+    location : FLocation,
+    rotation : FRotation
+}
 
+impl Character {
+    pub fn new() -> Character {
+        let mut _loc = FLocation::new(0.0, 0.0, 0.0);
+        let mut _rot = FRotation::new(0.0, 0.0, 0.0, 0.0, 0.0, 
+        0.0, 0.0);
+
+        Character { pid : -1, name : "".to_string(), location: _loc, rotation: _rot }
+    }
+
+    pub fn Update(&mut self) {
+
+    }
 }
 
 
 pub struct CharacterManager {
-    
+    characterContainer : Vec<Character>
+}
+
+impl CharacterManager {
+    pub fn new() -> CharacterManager {
+        CharacterManager { characterContainer: Vec::new() }
+    }
 }
 
 impl Manager for CharacterManager {
