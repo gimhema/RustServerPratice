c2fdd6106ec11e6c3831ef6f4bbdefe9d5dcf147
gimhema <49871739+gimhema@users.noreply.github.com>
2024-02-13 00:41:40 +0900

    Update ClientCommandModule.rs
    .

--- MessagePrompt/MessagePrompt/src/PromptClient/ClientCommandModule.rs
+++ MessagePrompt/MessagePrompt/src/PromptClient/ClientCommandModule.rs
@@ -3,15 +3,27 @@ use super::PromptClientMainModule::*;
 
 
 pub enum PromptCommand {
+    COMMAND_REQUEST_DEFAULT,
     COMMAND_REQUEST_CONNECT,
     COMMAND_REQUEST_EXIT,    
 } 
 
-impl From<PromptCommand> for String {
+impl From<PromptCommand> for &str {
     fn from(header: PromptCommand) -> Self {
         match header {
-            PromptCommand::COMMAND_REQUEST_CONNECT => "connect".to_string(),
-            PromptCommand::COMMAND_REQUEST_EXIT => "exit".to_string(),
+            PromptCommand::COMMAND_REQUEST_DEFAULT => "",
+            PromptCommand::COMMAND_REQUEST_CONNECT => "connect",
+            PromptCommand::COMMAND_REQUEST_EXIT => "exit",
+        }
+    }
+}
+
+impl From<&str> for PromptCommand {
+    fn from(command : &str) -> Self {
+        match command {
+            "connect" => PromptCommand::COMMAND_REQUEST_CONNECT,
+            "exit" => PromptCommand::COMMAND_REQUEST_CONNECT,
+            &_ => PromptCommand::COMMAND_REQUEST_DEFAULT,
         }
     }
 }
@@ -19,6 +31,24 @@ impl From<PromptCommand> for String {
 
 impl PromptClientMain {
     pub fn ExecuteCommand(&mut self, _command : PromptCommand) {
+        match _command {
+            PromptCommand::COMMAND_REQUEST_DEFAULT => {
+
+            },
+            PromptCommand::COMMAND_REQUEST_CONNECT => {
+                self.PromptRequestConnect();
+            },
+            PromptCommand::COMMAND_REQUEST_EXIT => {
+                self.PromptExit();
+            }
+        }
+    }
+
+    pub fn PromptExit(&mut self) {
+        self.Exit();        
+    }
+
+    pub fn PromptRequestConnect(&mut self ) {
 
     }
 }
