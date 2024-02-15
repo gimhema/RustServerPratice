4c6234a6fe8ae01d785ba5108040ba55429e8639
gimhema <49871739+gimhema@users.noreply.github.com>
2024-02-16 00:15:19 +0900

    Create AsyncConnectWorker class
    .

---
+++ UnrealClient/UnrealArenaClient/Source/UnrealArenaClient/Network/AsyncConnectWorker.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AsyncConnectWorker.h"
+
+
+#pragma region Main Thread Code
+// This code will be run on the thread that invoked this thread (i.e. game thread)
+
+
+FAsyncConnectWorker::FAsyncConnectWorker(/* You can pass in inputs here */)
+{
+	// Constructs the actual thread object. It will begin execution immediately
+	// If you've passed in any inputs, set them up before calling this.
+	Thread = FRunnableThread::Create(this, TEXT("Give your thread a good name"));
+}
+
+
+FAsyncConnectWorker::~FAsyncConnectWorker()
+{
+	if (Thread)
+	{
+		// Kill() is a blocking call, it waits for the thread to finish.
+		// Hopefully that doesn't take too long
+		Thread->Kill();
+		delete Thread;
+	}
+}
+
+
+#pragma endregion
+// The code below will run on the new thread.
+
+
+bool FAsyncConnectWorker::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("My custom thread has been initialized"))
+
+		// Return false if you want to abort the thread
+		return true;
+}
+
+
+uint32 FAsyncConnectWorker::Run()
+{
+	// Peform your processor intensive task here. In this example, a neverending
+	// task is created, which will only end when Stop is called.
+	while (bRunThread)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("My custom thread is running!"))
+			FPlatformProcess::Sleep(1.0f);
+	}
+
+	return 0;
+}
+
+
+// This function is NOT run on the new thread!
+void FAsyncConnectWorker::Stop()
+{
+	// Clean up memory usage here, and make sure the Run() function stops soon
+	// The main thread will be stopped until this finishes!
+
+	// For this example, we just need to terminate the while loop
+	// It will finish in <= 1 sec, due to the Sleep()
+	bRunThread = false;
+}
---
+++ UnrealClient/UnrealArenaClient/Source/UnrealArenaClient/Network/AsyncConnectWorker.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HAL/Runnable.h"
+
+/**
+ * 
+ */
+
+class UNREALARENACLIENT_API FAsyncConnectWorker : public FRunnable
+{
+public:
+
+	// Constructor, create the thread by calling this
+	FAsyncConnectWorker();
+
+	// Destructor
+	virtual ~FAsyncConnectWorker() override;
+
+
+	// Overriden from FRunnable
+	// Do not call these functions youself, that will happen automatically
+	bool Init() override; // Do your setup here, allocate memory, ect.
+	uint32 Run() override; // Main data processing happens here
+	void Stop() override; // Clean up any memory you allocated here
+
+
+private:
+
+	// Thread handle. Control the thread using this, with operators like Kill and Suspend
+	FRunnableThread* Thread;
+
+	// Used to know when the thread should exit, changed in Stop(), read in Run()
+	bool bRunThread;
+	
+};
