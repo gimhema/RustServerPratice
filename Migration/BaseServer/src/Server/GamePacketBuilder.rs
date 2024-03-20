30955ddce38e2b0d8faccfc29b6a966b2a603460
gimhema <49871739+gimhema@users.noreply.github.com>
2024-03-20 23:36:18 +0900

    Update GamePacketBuilder.rs
    .

--- Migration/BaseServer/src/Server/GamePacketBuilder.rs
+++ Migration/BaseServer/src/Server/GamePacketBuilder.rs
@@ -1,1 +1,23 @@
 use bincode::{config, Decode, Encode};
+
+
+// Struct -> Binary Packet
+macro_rules! encode_packet {
+    ($packet:expr, $config:expr) => {
+        {
+            bincode::encode_to_vec(&$packet, $config).unwrap()
+        }
+    };
+}
+
+
+// Binary Packet -> Struct
+macro_rules! decode_packet {
+    ($encoded:expr, $Type: expr ,$config:expr) => {
+        {
+            let (decoded, len): (Type, usize) = bincode::decode_from_slice(&$encoded[..], $config).unwrap();
+            (decoded, len)
+        }
+    };
+}
+
